659cb672c33accc1494c7029ad77a9f5803449ad
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2025-07-14 21:58:23 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_LzVOb79tlmQR@ep-rapid-voice-ac6uplh8-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require',
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
